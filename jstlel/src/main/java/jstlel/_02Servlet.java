af36246590cd4270c8f08d42bb0462eb9444d1af
rhkddms3501 <rhkddms3501@naver.com>
2023-01-19 16:18:15 +0900

    Servlet name modify

--- jstlel/src/main/java/jstlel/_02Servlet.java
+++ jstlel/src/main/java/jstlel/_02Servlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class _02Servelt extends HttpServlet {
+public class _02Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
